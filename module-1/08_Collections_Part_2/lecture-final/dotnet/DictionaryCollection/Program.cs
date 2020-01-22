b83c7843fb00e290d4c5bb9e1f79d44b353a7214
Katie Dwyer <katie.dwyer@techelevator.com>
2020-01-22 11:17:51 -0500

    08 class lecture and student exercise

---
+++ module-1/08_Collections_Part_2/lecture-final/dotnet/DictionaryCollection/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Person / Height Database");
+            Console.WriteLine();
+
+            Console.Write("Would you like to enter another person (yes/no)? ");
+            string input = Console.ReadLine().ToLower();
+
+            // 1. Let's create a new Dictionary that could hold string, ints
+            //      | "Josh"    | 70 |
+            //      | "Bob"     | 72 |
+            //      | "John"    | 75 |
+            //      | "Jack"    | 73 |
+            Dictionary<string, int> personHeight = new Dictionary<string, int>();
+
+            while (input == "yes" || input == "y")
+            {
+                Console.Write("What is the person's name?: ");
+                string name = Console.ReadLine().ToLower();
+
+                Console.Write("What is the person's height (in inches)?: ");
+                int height = int.Parse(Console.ReadLine());
+
+                // 2. Check to see if that name is in the dictionary
+                //      bool exists = dictionaryVariable.ContainsKey(key)
+                bool exists = personHeight.ContainsKey(name);
+
+                if (!exists)
+                {
+                    Console.WriteLine($"Adding {name} with new value.");
+                    // 3. Put the name and height into the dictionary
+                    //      dictionaryVariable[key] = value;
+                    //      OR dictionaryVariable.Add(key, value);
+                    //personHeight[name] = height;
+                    personHeight.Add(name, height); 
+
+                }
+                else
+                {
+                    Console.WriteLine($"Overwriting {name} with new value.");
+                    // 4. Overwrite the current key with a new value
+                    //      dictionaryVariable[key] = value;
+                    personHeight[name] = height;
+                }
+
+
+                Console.WriteLine();
+                Console.Write("Would you like to enter another person (yes/no)? ");
+                input = Console.ReadLine().ToLower();
+            }
+
+            Console.Write("Type \"all\" to print all names OR \"search\" to print out single name: ");
+            input = Console.ReadLine().ToLower();
+
+            if (input == "search")
+            {
+                Console.Write("Which name are you looking for? ");
+                input = Console.ReadLine().ToLower();
+
+                //5. Let's get a specific name from the dictionary
+                if (personHeight.ContainsKey(input))
+                {
+                    int height = personHeight[input];
+                    Console.WriteLine($"Found key {input}. Height is {height}");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} not found"); 
+                }
+
+            }
+            else if (input == "all")
+            {
+                Console.WriteLine();
+                Console.WriteLine(".... printing ...");
+
+                //6. Let's print each item in the dictionary
+                PrintDictionary(personHeight);
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Done...");
+
+            //7. Let's get the average height of the people in the dictionary
+            //make sure that we remember to do double division
+            double total = 0;
+            int count = 0;
+            foreach( int height in personHeight.Values)
+            {
+                total += height;
+                count++; 
+            }
+            Console.WriteLine($"Average height is {total / count}");
+            Console.WriteLine($"Count keys: {personHeight.Keys.Count} Count values: {personHeight.Values.Count}");
+
+
+        }
+
+        static void PrintDictionary(Dictionary<string, int> database)
+        {
+            // Looping through a dictionary involves using a foreach loop
+            // to look at each item, which is a key-value pair
+            foreach (KeyValuePair<string, int> kvp in database)
+            {
+                string name = kvp.Key;
+                int height = kvp.Value;
+                Console.WriteLine($"{name}: {height}");
+            }
+        }
+    }
+}
