ed1be1e26581e82d4ea51feea0f1a65b1d85c876
Katie Dwyer <katie.dwyer@techelevator.com>
2020-01-23 12:13:14 -0500

    09 class lecture and exercise

---
+++ module-1/09_Introduction_Classes/lecture-final/dotnet/DeckOfCards/Classes/Card.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards.Classes
+{
+    class Card
+    {
+        /// <summary>
+        /// This is going to be the suit - it'll be hearts, clubs, spades, diamonds all in lowercase
+        /// </summary>
+        public string Suit { get; }
+
+        public Card Copy()
+        {
+            Card theNewOne = new Card(Value, Suit, IsFaceUp);
+            return theNewOne; 
+        }
+
+        /// <summary>
+        /// store jack as 11, queen as 12, king as 13 aces are 1
+        /// </summary>
+        public int Value { get; }
+
+        public bool IsFaceUp { get; private set; }
+
+        public void Flip()
+        {
+            // IsFaceUp = !IsFaceUp;
+            if (IsFaceUp) //if its face up flip it over
+                IsFaceUp = false;
+            else 
+                IsFaceUp = true;
+        }
+
+
+        public Card(int value, string suit)
+        {
+            //value = Value; not this
+            Value = value;
+            Suit = suit;
+            IsFaceUp = false; //this is the default, adding this line improves readability and maintainability
+        }
+
+        public Card(int value, string suit,bool isFaceUp)
+        {
+            //value = Value; not this
+            Value = value;
+            Suit = suit;
+            IsFaceUp = isFaceUp;
+        }
+
+        public bool BeatsCard(Card otherOne)
+        {
+            if (Value > otherOne.Value)
+                return true;
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            if(IsFaceUp)
+            {
+                string strVal = Value.ToString();
+                if (Value==11)
+                {
+                    strVal = "Jack";
+                }
+                else if (Value == 12)
+                {
+                    strVal = "Queen";
+                }
+                else if (Value == 13)
+                {
+                    strVal = "King";
+                }
+                else if (Value == 1)
+                {
+                    strVal = "Ace";
+                }
+                return strVal + " of " + Suit;
+            }
+            else
+            {
+                return "back of card";
+            }
+        }
+
+    }
+}
---
+++ module-1/09_Introduction_Classes/lecture-final/dotnet/DeckOfCards/Program.cs
@@ -0,0 +1,100 @@
+﻿using DeckOfCards.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card fiveOfSpades = new Card(5, "Spades");
+            Card twoOfDiamons = new Card(2, "Diamonds");
+
+            Card copyOffiveOfSpades = fiveOfSpades.Copy(); 
+
+            if (fiveOfSpades.BeatsCard(twoOfDiamons))
+            {
+                Console.WriteLine($"{fiveOfSpades} wins");
+            }
+            else
+            {
+                Console.WriteLine($"{twoOfDiamons} wins");
+            
+            }
+
+            // Let's store all of our cards in a list
+            // CODE GOES HERE
+            List<Card> deck = new List<Card>();
+
+            while (true)
+            {
+                Console.WriteLine("What do you want to do? ");
+                Console.WriteLine("1. Create a new card.");
+                Console.WriteLine("2. Display all of the cards.");
+                Console.WriteLine("3. Flip all of the cards.");
+                Console.WriteLine("Q. Quit");
+
+                /*
+                Card x = new Card(); 
+                x.Flip();  allowed since Flip is public
+                x.IsFaceUp = true; //not allowed since it's private set and we aren't inside of class
+                */
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    // Get the value for the new card
+                    Console.Write("What is the value of the card (1-13): ");
+                    int value = int.Parse(Console.ReadLine());
+
+                    // Get the suit for the new card
+                    Console.Write("What suit does the card have (Hearts, Diamonds, Clubs, Spades): ");
+                    string suit = Console.ReadLine();
+
+                    // Is the card face up or face down
+                    Console.Write("Is the card face up (True/False): ");
+                    bool isFaceUp = bool.Parse(Console.ReadLine());
+
+                    // Create the card and add to the list
+                    Card card = new Card(value, suit, isFaceUp);
+                    deck.Add(card);
+                    
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Displaying all of the cards.");
+
+                    // Loop through each of the cards
+                    foreach(Card card in deck)
+                    {
+                        Console.WriteLine(card);
+                    }
+                    Console.WriteLine("press enter to continue");
+                    Console.ReadLine();
+                    
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine($"Flipping the cards.");
+                    
+                    // Loop through each of the cards and flip them
+                    foreach(Card c in deck)
+                    {
+                        c.Flip(); 
+                    }
+                    
+                }
+                else if (input == "Q" || input=="q")
+                {
+                    break;
+                }
+
+                // Wait for user to press enter and clear screen.
+               
+                Console.Clear();
+            }
+        }
+    }
+}
