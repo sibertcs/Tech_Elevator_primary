d9adbf1e4a9fe02285add1402c2f8b7eb07a5594
Katie Dwyer <katie.dwyer@techelevator.com>
2020-01-24 11:42:00 -0500

    10 lecture final and student exercise

---
+++ module-1/10_Classes_Encapsulation/lecture-final/dotnet/DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,34 @@
+﻿using DeckOfCards.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Card c = new Card(); forbidden
+
+
+            Deck deck = new Deck();
+            deck.Shuffle();
+            deck.Shuffle(5);
+
+            List<Card> katiesHand = new List<Card>();
+            List<Card> irvingHand = new List<Card>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                katiesHand.Add(deck.DealOne());
+                irvingHand.Add(deck.DealOne());
+            }
+            Console.WriteLine("Katies Hand");
+            foreach(Card c in katiesHand)
+            {
+                Console.Write(c+" "); 
+            }
+        }
+    }
+}
