4d9a6880adfbf54c63144b3088aea02666af6bdd
Katie Dwyer <katie.dwyer@techelevator.com>
2020-01-27 11:39:20 -0500

    11 inheritance lecture final

---
+++ module-1/11_Inheritance/lecture-final/dotnet/InheritanceLecture/Auctioneering/BuyoutAuction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceLecture.Auctioneering
+{
+    /// <summary>
+    /// a buyout auction has a price that the bidder can use allowing the auction to end
+    /// </summary>
+    public class BuyoutAuction : Auction
+    {
+        private int buyoutPrice; 
+        public int BuyoutPrice
+        {
+            get
+            {
+                return buyoutPrice; 
+            }
+        }
+
+
+        //if i don't explicitly call a parent constructor, compiler calls the default no-arg constructor first
+        public BuyoutAuction(int buyoutPrice) : this (buyoutPrice,"BuyoutAuction")
+        {
+            this.buyoutPrice = buyoutPrice;
+            Console.WriteLine("In buyout Constructor");
+        }
+
+        public BuyoutAuction(int buyoutPrice, string name) : base(name)
+        {
+            this.buyoutPrice = buyoutPrice;
+            Console.WriteLine("In buyout Constructor");
+        }
+
+        public void KatieAutoBid()
+        {
+            Bid katieBid = new Bid("Katie", 50);
+            base.PlaceBid(katieBid);
+        }
+
+        public override bool PlaceBid(Bid offeredBid)
+        {
+            bool isCurrentWinningBid = false;
+            //return base.PlaceBid(offeredBid);
+
+            if (!HasEnded)//if the auction is still going on
+            {
+                //if the offered bid amount is highter than the buyout price
+                if (offeredBid.BidAmount >= buyoutPrice)
+                {
+                    Bid newBid = new Bid(offeredBid.Bidder, buyoutPrice);
+                    base.PlaceBid(newBid);
+                    Console.WriteLine("Buyout met by " + newBid.Bidder);
+                    base.EndAuction();
+                    isCurrentWinningBid = true;
+                }
+                else
+                {
+                    isCurrentWinningBid = base.PlaceBid(offeredBid);
+                }
+
+               
+            }
+
+
+            return isCurrentWinningBid;
+        }
+    }
+}
---
+++ module-1/11_Inheritance/lecture-final/dotnet/InheritanceLecture/Program.cs
@@ -0,0 +1,50 @@
+﻿using InheritanceLecture.Auctioneering;
+using System;
+
+namespace InheritanceLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new general auction
+            Console.WriteLine("Starting a general auction");
+            Console.WriteLine("-----------------");
+
+            /*
+            Auction generalAuction = new Auction();
+            Console.WriteLine("general auction: " + generalAuction);
+
+            generalAuction.PlaceBid(new Bid("Josh", 1));
+            generalAuction.PlaceBid(new Bid("Fonz", 23));
+            generalAuction.PlaceBid(new Bid("Rick Astley", 13));
+
+            Console.WriteLine("Current high bid: "+generalAuction.CurrentHighBid);
+            //....
+            //....
+            // This might go on until the auction runs out of time or hits a max # of bids
+            */
+
+            /*
+            ReserveAuction a = new ReserveAuction(50);
+            a.PlaceBid(new Bid("Zach", 2));
+            a.PlaceBid(new Bid("Chaz", 3));
+            a.PlaceBid(new Bid("NOel", 4));
+            a.PlaceBid(new Bid("Caleb", 51));
+            a.PlaceBid(new Bid("Xanth", 1000000));
+            a.PlaceBid(new Bid("Allen", 1));
+            */
+
+            BuyoutAuction buyout = new BuyoutAuction(50);
+            buyout.PlaceBid(new Bid("Zach", 2));
+            buyout.PlaceBid(new Bid("Chaz", 3));
+            buyout.PlaceBid(new Bid("NOel", 4));
+            buyout.PlaceBid(new Bid("Caleb", 51));
+            buyout.PlaceBid(new Bid("Xanth", 1000000));
+            buyout.PlaceBid(new Bid("Allen", 1));
+
+            BuyoutAuction katieBuyout = new BuyoutAuction(100,"Katies fantastic auction");
+            Console.WriteLine("buyout name " + buyout.Name + "katieBuyout name is " + katieBuyout.Name);
+        }
+    }
+}
