adef892ac1b92d976cb02f8895b3dd66890b5f5f
Katie Dwyer <katie.dwyer@techelevator.com>
2020-01-31 08:52:40 -0500

    16 TDD student lecture

---
+++ module-1/16_Test_Driven_Development/student-lecture/dotnet/TDD.Tests/Classes/KataRomanNumeralTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD.Classes;
+
+namespace TDD.Tests.Classes
+{
+    [TestClass]
+    public class KataRomanNumeralTests
+    {
+       
+    }
+}
