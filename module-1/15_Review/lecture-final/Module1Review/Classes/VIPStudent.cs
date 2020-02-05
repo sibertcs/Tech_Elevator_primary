24d753c7c09f32da98ea61a667fec2fe40c412f4
Katie Dwyer <katie.dwyer@techelevator.com>
2020-02-05 11:49:42 -0500

    15 review

---
+++ module-1/15_Review/lecture-final/Module1Review/Classes/VIPStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module1Review.Classes
+{
+    public class VIPStudent : Student
+    {
+        //this class to return name + " the Magnificent" for the name
+        public override string Name { 
+            get
+            {
+                return base.Name + " the Magnificent"; 
+            }
+        }
+
+        public VIPStudent(string name) : base(name)
+        {
+            quizScores.Add(100);
+        }
+        public VIPStudent() { }
+
+        public override void TakeQuiz()
+        {
+            //generate a random number between 0 and 100
+            int score = random.Next(70, 100);
+
+            //add as a quiz score
+            AddQuizScore(score);
+        }
+    }
+}
