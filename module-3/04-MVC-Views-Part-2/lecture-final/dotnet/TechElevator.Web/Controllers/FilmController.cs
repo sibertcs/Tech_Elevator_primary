b862b2b3c5635bde2c4a01324900399cd00c3ecc
Katie Dwyer <katie.dwyer@techelevator.com>
2020-02-27 10:56:43 -0500

    m3 04 class lecture and exercise

---
+++ module-3/04-MVC-Views-Part-2/lecture-final/dotnet/TechElevator.Web/Controllers/FilmController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechElevator.Web.DAL;
+
+namespace TechElevator.Web.Controllers
+{
+    public class FilmController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            IStarWarsDAO dao = new MockStarWarsDAO();
+            var films = dao.GetFilms();
+            return View("Index", films);
+        }
+
+        public IActionResult Detail(string id)
+        {
+            //get the film with the corresponding id and show the details
+            IStarWarsDAO dao = new MockStarWarsDAO();
+            var film = dao.GetFilm(id);
+            return View("Detail",film);
+        }
+    }
+}
