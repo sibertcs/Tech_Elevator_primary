cc01df409c2f4d76fa9ef680c5c8267672ec7fff
Katie Dwyer <katie.dwyer@techelevator.com>
2020-02-28 11:09:59 -0500

    m3 05 class lecture

---
+++ module-3/05-MVC-Views-Part-3/lecture-final/dotnet/TechElevator.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechElevator.Web.Models;
+
+namespace TechElevator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {           
+           
+            return View();
+        }
+
+        public IActionResult SnackDetail(string name, int numCalories)
+        {
+            ViewData["g"] = "happy friday";
+            ViewData["1"] = "happy saturday";
+            Cookie chocolateCHip = new Cookie(name, numCalories);
+            return View(chocolateCHip);
+        }
+    }
+}
---
+++ module-3/05-MVC-Views-Part-3/lecture-final/dotnet/TechElevator.Web/Models/Cookie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechElevator.Web.Models
+{
+    public class Cookie
+    {
+        public string Type { get; }
+        public int numCalories { get; }
+
+        public Cookie(string type, int nc)
+        {
+            Type = type;
+            numCalories=nc;
+                 
+        }
+    }
+}
