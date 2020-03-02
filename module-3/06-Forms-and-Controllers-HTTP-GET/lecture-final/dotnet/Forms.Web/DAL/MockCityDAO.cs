9e585749e49fbd015f87b4b4c2676802805d0de8
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-02 11:57:26 -0500

    M3 06 class lecture and homework

---
+++ module-3/06-Forms-and-Controllers-HTTP-GET/lecture-final/dotnet/Forms.Web/DAL/MockCityDAO.cs
@@ -0,0 +1,64 @@
+﻿using Forms.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.DAL
+{
+    public class MockCityDAO : ICityDAO
+    {
+        void ICityDAO.AddCity(City city)
+        {
+            throw new NotImplementedException();
+        }
+
+        IList<City> ICityDAO.GetCities()
+        {
+            IList<City> myList= new List<City>();
+            City oz = new City();
+            oz.CityId = 1;
+            oz.CountryCode = "USA";
+            oz.District = "Kansas";
+            oz.Population = 10;
+            oz.Name = "Oz";
+            myList.Add(oz);
+            City b = new City();
+            b.CityId = 1;
+            b.CountryCode = "USA";
+            b.District = "Florida";
+            b.Population = 0;
+            b.Name = "Atlantis";
+            myList.Add(b);
+
+            return myList;
+        }
+
+        IList<City> ICityDAO.GetCities(string countryCode, string district)
+        {
+            IList<City> myList = new List<City>();
+            City oz = new City();
+            oz.CityId = 1;
+            oz.CountryCode = "USA";
+            oz.District = "Kansas";
+            oz.Population = 10;
+            oz.Name = "Oz";
+            myList.Add(oz);
+            City b = new City();
+            b.CityId = 1;
+            b.CountryCode = "USA";
+            b.District = "Florida";
+            b.Population = 0;
+            b.Name = "Atlantis";
+            myList.Add(b);
+
+            return myList;
+        }
+
+        IList<string> ICityDAO.GetCountryCodes()
+        {
+            IList<string> myList = new List<string>() { "USA", "GBR", "CHN" };
+            return myList;
+        }
+    }
+}
