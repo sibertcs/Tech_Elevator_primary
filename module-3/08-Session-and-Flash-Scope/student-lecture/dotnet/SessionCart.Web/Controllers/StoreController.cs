e3720afbec9543b3953458f0da5f722f002a8ed6
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-04 08:58:23 -0500

    m3 08 session student lecture

---
+++ module-3/08-Session-and-Flash-Scope/student-lecture/dotnet/SessionCart.Web/Controllers/StoreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SessionCart.Web.DAL;
+using SessionCart.Web.Models;
+using SessionCart.Web.Extensions;
+
+namespace SessionCart.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        /* Steps
+          
+       */
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
