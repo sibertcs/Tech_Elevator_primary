fe9e52e253dcf6e0aae98623e9886397081845a7
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-05 09:07:52 -0500

    09 student lecture

---
+++ module-3/09-Data-Validation-and-View-Models/student-lecture/dotnet/Forms.Web/Models/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Models
+{
+    public class City
+    {
+        public int CityId { get;  set; }
+        
+        public string Name { get;  set; }
+
+        public string CountryCode { get;  set; }
+
+        public string District { get;  set; }
+
+        public int Population { get;  set; }
+    }
+}
