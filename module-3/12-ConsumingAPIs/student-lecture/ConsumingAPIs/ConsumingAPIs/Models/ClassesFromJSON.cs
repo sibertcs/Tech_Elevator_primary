fa986d4d52f2ae48af192916f2d4c85a991751c1
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-13 09:23:24 -0400

    m3 12 consuming apis in mvc

---
+++ module-3/12-ConsumingAPIs/student-lecture/ConsumingAPIs/ConsumingAPIs/Models/ClassesFromJSON.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumingAPIs.Models
+{
+    //What is this magic? 
+    //Copy your JSON, go to edit->Paste Special
+    //and then Paste JSON as classes
+    //Then you can rename if you want to
+
+    public class Rootobject
+    {
+        public Fact[] all { get; set; }
+    }
+
+    public class Fact
+    {
+        public string _id { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+        public User user { get; set; }
+        public int upvotes { get; set; }
+        public object userUpvoted { get; set; }
+    }
+
+    public class User
+    {
+        public string _id { get; set; }
+        public Name name { get; set; }
+    }
+
+    public class Name
+    {
+        public string first { get; set; }
+        public string last { get; set; }
+    }
+
+}
---
+++ module-3/12-ConsumingAPIs/student-lecture/ConsumingAPIs/ConsumingAPIs/Models/SearchImageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumingAPIs.Models
+{
+    public class SearchImageViewModel
+    {
+        public int SelectedValue { get; set; } = 2;
+
+       public SelectList ImageCategories= new SelectList(
+        new List<SelectListItem>
+        {
+           new SelectListItem { Selected = false,  Text = "boxes", Value = "5"},
+           new SelectListItem { Selected = false,  Text = "clothes", Value = "15"},
+           new SelectListItem { Selected = false,  Text = "hats", Value = "1"},
+           new SelectListItem { Selected = false,  Text = "sinks", Value = "14"},
+           new SelectListItem { Selected = false,  Text = "space", Value = "2"},
+           new SelectListItem { Selected = false,  Text = "sunglasses", Value = "4"},
+           new SelectListItem { Selected = false,  Text = "ties", Value = "7"},
+        }, "Value" , "Text", 1);
+    }
+}
