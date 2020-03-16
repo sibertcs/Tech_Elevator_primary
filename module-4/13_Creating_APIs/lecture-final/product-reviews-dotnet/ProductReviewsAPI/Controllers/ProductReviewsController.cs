b3242cc1e1049647f45e63fd41cba3fff3755988
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-16 10:13:48 -0400

    M4 13 API Break 1

---
+++ module-4/13_Creating_APIs/lecture-final/product-reviews-dotnet/ProductReviewsAPI/Controllers/ProductReviewsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductReviewsAPI.Models;
+using ProductReviewsAPI.Services;
+
+namespace ProductReviewsAPI.Controllers
+{
+    [Route("api/reviews")]
+    [ApiController]
+    public class ProductReviewsController : ControllerBase
+    {
+        private IDataAccessLayer<ProductReview> dal;
+
+        public ProductReviewsController(IDataAccessLayer<ProductReview> dal)
+        {
+            this.dal = dal;
+        }
+    }
+}
