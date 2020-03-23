e2c3524a4e414f6cee8d866ea10bdeb62316f73b
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-23 15:28:23 -0400

    review

---
+++ module-4/05_Review/LinkedList/LinkedList/Models/LinkedList_int.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Models
+{
+    class LinkedList_int
+    {
+        private LinkedListNode_int start = null;
+        private LinkedListNode_int end = null;
+
+        private void AddToBeginning(LinkedListNode_int lln)
+        {
+            if (start == null)
+            {
+                start = lln;
+                end = lln;
+            }
+            else
+            {
+                lln.next = start;
+                start = lln;
+            }
+        }
+        public void AddToBeginning(int newInt)
+        {
+            LinkedListNode_int newNode = new LinkedListNode_int(newInt);
+            AddToBeginning(newNode);
+
+        }
+        /// <summary>
+        /// this method traverses our entire linked list and prints all the data
+        /// </summary>
+        public void printAll()
+        {
+            LinkedListNode_int thisNode = start;
+            while (thisNode != null)
+            {
+                Console.WriteLine(thisNode.Data);
+                thisNode = thisNode.next; 
+            }
+        }
+
+    }
+}
---
+++ module-4/05_Review/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,37 @@
+﻿using LinkedList.Models;
+using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList_int myList = new LinkedList_int();
+            myList.AddToBeginning(5);
+            myList.AddToBeginning(15);
+            myList.AddToBeginning(-1);
+            myList.AddToBeginning(7);
+
+            myList.printAll();
+
+            //Vehicle a = new Vehicle(); Vehicle is abstract
+
+            Vehicle v = new Car();
+            v.Move();
+
+            Vehicle h = null;
+            bool shouldBeBoat = true;
+            if (shouldBeBoat)
+            {
+                h = new Boat();
+            }
+            else
+            {
+                h = new Car();
+            }
+            h.Move();
+        }
+    }
+}
