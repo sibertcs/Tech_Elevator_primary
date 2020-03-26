d82bbe1547f0562f3491c73c9ed64ca26665e825
Caleb Sibert <sibertcs@gmail.com>
2020-03-26 19:36:55 -0400

    Submitting Intro to Vue Exerices

---
+++ module-4/06_Introduction_to_Vue/student-exercise/shopping-list/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <shopping-list></shopping-list>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from '@/components/ShoppingList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+    ShoppingList
+  }
+}
+</script>
+
+<style>
+body {
+  background: rgb(61,201,164);
+  background: radial-gradient(circle, rgba(61,201,164,1) 50%, rgba(92,133,120,1) 100%);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ module-4/06_Introduction_to_Vue/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="shopping-list">
+  <h1>My Shopping List</h1>
+  <ul>
+      <li>Bread</li>
+      <li class='completed'>Eggs</li>
+      <li>Milk</li>
+      <li>Coffee</li>
+      <li>Bananas</li>
+      <li>Oranges</li>
+      <li>Tomatoes</li>
+      <li class='completed'>Salmon</li>
+      <li>Juice</li>
+      <li>Spaghetti</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'shopping-list'
+}
+</script>
+
+<style>
+.shopping-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#5c8578;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:last-child{
+    border:0px;
+}
+li.completed {
+    text-decoration: line-through;
+    color:darkgrey;
+}
+</style>
