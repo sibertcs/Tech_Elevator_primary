3a17d80e4fadd51d5d9ee2942fa032e28039b183
Katie Dwyer <katie.dwyer@techelevator.com>
2020-03-26 11:40:49 -0400

    m4 06 vue intro class lecture and homework

---
+++ module-4/06_Introduction_to_Vue/lecture-final/holy-grail-layout/src/components/MainContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <article>cOMPONENT Article</article>
+</template>
+
+<script>
+export default {
+    name: 'main-content'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-4/06_Introduction_to_Vue/lecture-final/vue-product-reviews/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">   
+   
+     <product-review></product-review>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import ProductReview from '@/components/ProductReview'
+
+export default {
+  name: 'App',
+  components: {
+
+    ProductReview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
